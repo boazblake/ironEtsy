63b3941483110475bf4af76bee9dbe1edea93de9
Boaz Blake <boazblake@gmail.com>
2016-03-11 11:30:22 -0600

    added dynamic search functunality

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ console.log(Backbone)
 console.log(window.location.hash)
 var etsyKey = 'uhdk37cf5h6h7dcxamb6eb9x'
 var url = 'https://openapi.etsy.com/v2/listings/active?'
+var searchBar = document.querySelector('#searchBar')
+
 
 // Router
 var ironEtsyRouter = Backbone.Router.extend({
+
     routes: {
         'details/:id': 'handle_Detail_Data',
         'search/:keywords': 'handle_Search_Data',
         '*home': 'handle_Default_Data'
     },
+
 
     handle_Detail_Data: function(searchInput) {
         var etsy_Detail_Model = new defaultModel()
@@ -18,23 +22,11 @@ var ironEtsyRouter = Backbone.Router.extend({
         etsy_Detail_Model.fetch({
 
             data: {
-                // search: keywords,
                 api_key: etsy_Detail_Model.api_key
             }
         })
     },
 
-    handle_Default_Data: function() {
-        var etsy_Default_Model = new defaultModel()
-        var etsy_Default_View = new defaultView(etsy_Default_Model)
-        etsy_Default_Model.fetch({
-
-            data: {
-                // search: keywords,
-                api_key: etsy_Default_Model.api_key
-            }
-        })
-    },
 
     handle_Search_Data: function(keywords) {
         var etsy_Search_Model = new defaultModel()
@@ -48,6 +40,21 @@ var ironEtsyRouter = Backbone.Router.extend({
             }
         })
     },
+
+
+    handle_Default_Data: function() {
+        var etsy_Default_Model = new defaultModel()
+        var etsy_Default_View = new defaultView(etsy_Default_Model)
+        etsy_Default_Model.fetch({
+
+            data: {
+                // search: keywords,
+                api_key: etsy_Default_Model.api_key
+            }
+        })
+    },
+
+
 
     initialize: function() {
         Backbone.history.start()
@@ -73,25 +80,14 @@ var defaultModel = Backbone.Model.extend({
 
 
 
+
+
+
+
 // View
-var searchView = Backbone.Model.extend({
-    el: '#container',
-
-    initialize: function(aModel) {
-        this.model = aModel
-        var boundRender = this._render.bind(this)
-        this.model.on('sync', boundRender)
-    },
-
-    _render: function() {
-        console.log(data)
-            // console.log(this.model.data.get('data'))
-    }
-
-})
 
 var detailView = Backbone.Model.extend({
-    el: '#container',
+    el: '.bod',
 
     initialize: function(aModel) {
         this.model = aModel
@@ -107,18 +103,58 @@ var detailView = Backbone.Model.extend({
 
 })
 
+var searchView = Backbone.Model.extend({
+    el: '.bod',
+
+    initialize: function(aModel) {
+        this.model = aModel
+        var boundRender = this._render.bind(this)
+        this.model.on('sync', boundRender)
+    },
+
+    _render: function() {
+            // console.log(this.model.data.get('data'))
+    }
+
+})
+
 var defaultView = Backbone.View.extend({
-        el: '#container',
+        el: '.bod',
 
         initialize: function(aModel) {
             this.model = aModel
             var boundRender = this._render.bind(this)
             this.model.on('sync', boundRender)
-            console.log(this.model)
-
         },
 
-        _render: function() {}
+        events:{
+         // «evtType»  «domEL-selector» :  »
+        	'keypress #searchBar':'_runSearch'
+        },
+
+        _runSearch:function(keypress){
+        	console.log(keypress.target)
+
+        	window.location.hash = 'search/' + keypress.target.etsyid
+        },
+
+        _render: function() {
+            var listOfDescriptions = this.model.attributes.results
+            listOfTitles = ''
+            this.el.innerHTML = '<div class="hed">'
+            this.el.innerHTML += 	'<input id="searchBar" type="textarea" placeholder="search Iron Etsy...">'
+            this.el.innerHTML += '</div>'
+            
+           
+            // console.log(this.model.get('listing_id'))
+            for (var i = 0; i < listOfDescriptions.length; i++){
+            	listOfTitles = listOfDescriptions[i].title
+            	etsyID = listOfDescriptions[i].listing_id
+            	// console.log(listOfDescriptions[i].listing_id)
+            	this.el.innerHTML += '<li etsyID="'+ etsyID +'"">'+ listOfTitles +'</li>'
+            }
+
+        }
 
     })
     ////////////////////////////////////////////////////
